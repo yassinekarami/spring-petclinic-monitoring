7ad779ff804263ecb9acac3637a6b81369408aab
yassinekarami <yassine.karami@outlook.com>
2026-08-19 21:45:23 +0100

    Add output for cluster ID in Terraform configuration
    Signed-off-by: yassinekarami <yassine.karami@outlook.com>

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -3,3 +3,9 @@ output "kubeconfig" {
   sensitive   = true
   value       = scaleway_k8s_cluster.this.kubeconfig[0].config_file
 }
+
+output "cluster_id" {
+  description = "The cluster ID"
+  sensitive   = true
+  value       = scaleway_k8s_cluster.this.id
+}
