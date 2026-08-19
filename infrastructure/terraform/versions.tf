6de8b4d878afef3ade0102d4c30aafb5117c8772
yassinekarami <yassine.karami@outlook.com>
2026-08-19 21:27:05 +0100

    Remove sensitive credentials from versions.tf
    Removed access key and secret key from Terraform configuration.
    
    Signed-off-by: yassinekarami <yassine.karami@outlook.com>

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -11,8 +11,6 @@ terraform {
     endpoints = {
         s3 = "https://s3.fr-par.scw.cloud"
     }
-    access_key = var.access_key
-    secret_key = var.secret_key
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
