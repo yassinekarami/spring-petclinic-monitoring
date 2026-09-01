355affef9b8f15a3931f236f7e25a422583a8d1a
yassine <yassine.karami@outlook.com>
2026-09-01 22:06:12 +0200

    ajout access key

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -8,6 +8,7 @@ terraform {
     bucket = "terraform-s3"
     key = "terraform.tfstate"
     region   = "fr-par"
+    access_key = var.access_key
     endpoints = {
         s3 = "https://s3.fr-par.scw.cloud"
     }
